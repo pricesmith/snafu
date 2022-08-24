2be21dc51fdddb38bd6038a2bff4f1d1e90b0c51
Price Smith <psmith.code@gmail.com>
2022-08-24 03:19:15 -0400

    my airpods just died

---
+++ src/memtable/mt_factory.rs
@@ -0,0 +1,3 @@
+/// The factory object of memtable. By specifying factory object user can 
+/// change the underlying implementation of memtable, and provide implementation 
+/// specific options (Default: SkipListFactory).
